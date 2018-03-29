81db7331ed0602ef4b84873dd0b952fce7450660
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-03-28 20:35:24 -0500

    Basic JavaScript Pseudocode.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
 $( document ).ready(function() {
+//VARIABLES
 
-// end of document ready 
-});
+
+
+
+
+//FUNCTIONS
+
+
+
+
+//EVENTS
+    //Click of Movie Reference
+        //Hide Main Menu
+        //Show Movies Menu
+        //Movies generate based on list of Movies from API
+
+    //Click of Movie in Movie References
+    $(document).on("click", ".js-movie-button", function() {
+        //Calls up individual Movie Page
+        //Calls Wikipedia API for Description of Movie
+        //Return to Main Menu Button
+
+    })
+    //Click of Movie Quiz
+    $(document).on("click", ".js-movie-quiz-button", function() {
+        //Questions are Asked
+        //Responses and their effects on the answers are generated.
+        //Result or results are picked.
+        //Result page displays.
+        //Return to Main Menu Button
+    })
+    //Click of Spirit Quiz
+    $(document).on("click", ".js-spirit-quiz-button", function() {
+        //Questions are Asked
+        //Responses and their effects on the answers are generated.
+        //Result or results are picked.
+        //Result page displays.
+        //Return to Main Menu Button
+    })
+
+
+}); // Closing of Document Ready
 
 
 
