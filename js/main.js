d405e284f0bcebdfca941a52409099dfe7ee5306
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-03-29 08:16:47 -0500

    Added reset div and some basic menu functionality in the javascript file. Due to document.ready, I moved the js script to the top of the HTML after jQuery, and removed the empty script tag.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,28 @@ $( document ).ready(function() {
 
 
 //FUNCTIONS
+function init() {
+    $(".js-resetDiv").hide();
+}
 
+function hideMenu() {
+    $(".js-mainMenuDiv").hide();
+    $(".js-resetDiv").show();
+}
+
+function renderMovieRef() {
+
+}
+
+function mainMenu() {
+    $(".js-mainMenuDiv").show();
+    $(".js-resetDiv").hide();
+}
 
 
 
 //EVENTS
+init();
     //Click of Movie Reference
         //Hide Main Menu
         //Show Movies Menu
@@ -18,26 +35,35 @@ $( document ).ready(function() {
 
     //Click of Movie in Movie References
     $(document).on("click", ".js-movie-button", function() {
+        hideMenu();
         //Calls up individual Movie Page
         //Calls Wikipedia API for Description of Movie
         //Return to Main Menu Button
-
+        console.log("js-movie button success");
     })
     //Click of Movie Quiz
     $(document).on("click", ".js-movie-quiz-button", function() {
+        hideMenu();
         //Questions are Asked
         //Responses and their effects on the answers are generated.
         //Result or results are picked.
         //Result page displays.
         //Return to Main Menu Button
+        console.log("js-movie-quiz button success");
     })
     //Click of Spirit Quiz
     $(document).on("click", ".js-spirit-quiz-button", function() {
+        hideMenu();
         //Questions are Asked
         //Responses and their effects on the answers are generated.
         //Result or results are picked.
         //Result page displays.
         //Return to Main Menu Button
+        console.log("js-spirit-quiz button success");
+    })
+    //Return to Main Menu
+    $(document).on("click", ".js-resetDiv", function() {
+        mainMenu();
     })
 
 
