8adcda6e711914f3f01c218912a92e1a36c0242f
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-03-30 20:22:52 -0500

    Added API Call - Known issue with origin 'null' present

--- js/main.js
+++ js/main.js
@@ -16,7 +16,28 @@ function hideMenu() {
 }
 
 function renderMovieRef() {
+    let movieRefArray = ["Laputa: Castle in the Sky", "Grave of the Fireflies,", "My Neighbor Totoro", "Kiki's Delivery Service", "Only Yesterday", "Porco Rosso", "Ocean Waves", "Pom Poko", "Whisper of the Heart", "Princess Mononoke", "My Neighbors the Yamadas", "Spirited Away", "The Cat Returns", "Howl's Moving Castle", "Tales from Earthsea", "Ponyo", "Arrietty", "From Up on Poppy Hill", "The Wind Rises", "The Tale of the Princess Kaguya", "When Marnie Was There", "How Do You Live?"];
+    $(".js-movie-ref-div").empty();
+    for (let i = 0; i < movieRefArray.length; i++) {
+        var movieRefItem = $("<button>");
+        movieRefItem.addClass("js-movieRef");
+        movieRefItem.attr("data-name", movieRefArray[i]);
+        movieRefItem.text(movieRefArray[i]);
+        $(".js-movie-ref-div").append(movieRefItem);
+    }
+}
 
+function movieRefCall() {
+    let movieName = $(this).attr("data-name");
+    console.log(movieName);
+    console.log(this);
+    let queryBase ="https://ghibliapi.herokuapp.com/films";
+    $.ajax({
+        url: queryBase,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
 }
 
 function mainMenu() {
@@ -27,20 +48,17 @@ function mainMenu() {
 
 
 //EVENTS
-init();
-    //Click of Movie Reference
-        //Hide Main Menu
-        //Show Movies Menu
-        //Movies generate based on list of Movies from API
-
-    //Click of Movie in Movie References
+    init();
     $(document).on("click", ".js-movie-button", function() {
         hideMenu();
+        renderMovieRef();
+        console.log("js-movie button success");
+    })
+    //Click of Movie in Movie References
+    $(document).on("click", ".js-movieRef", movieRefCall);
         //Calls up individual Movie Page
         //Calls Wikipedia API for Description of Movie
         //Return to Main Menu Button
-        console.log("js-movie button success");
-    })
     //Click of Movie Quiz
     $(document).on("click", ".js-movie-quiz-button", function() {
         hideMenu();
@@ -65,6 +83,7 @@ init();
     $(document).on("click", ".js-resetDiv", function() {
         mainMenu();
     })
+    $(document).on("click", ".js-movieRef", movieRefCall);
 
 
 }); // Closing of Document Ready
